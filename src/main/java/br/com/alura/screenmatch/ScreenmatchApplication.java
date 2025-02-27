45faa7ffd98aa9f18db1b68f346703b2abdf8afe
BrendaDantas <brendasod98@gmail.com>
2025-02-27 13:42:51 -0300

    Como faremos uma aplicação de linha de comando, Implementando a interface de linha de comando.

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,24 @@
 package br.com.alura.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* A interface vai permitir que eu crie, dentro do meu método principal, algumas chamadas.
+* */
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	/*
+	* O método run funciona como um método main convencional.
+	* */
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello, World.");
+	}
 }
