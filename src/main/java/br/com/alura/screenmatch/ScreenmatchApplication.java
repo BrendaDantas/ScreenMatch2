1570b08bc806d49b6c464a818c5845cca64c5a1b
BrendaDantas <brendasod98@gmail.com>
2025-03-03 19:43:32 -0300

    Buscando dados completos da série.

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,8 @@
 package br.com.alura.screenmatch;
-
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /*
 * A interface vai permitir que eu crie, dentro do meu método principal, algumas chamadas.
@@ -27,41 +19,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	* */
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoApi consumoApi = new ConsumoApi();
 
-		/*
-		* Consumo API, obtenho os dados da série e atribuo para a variável json
-		* */
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=f7de918f");
-		System.out.println(json);
-
-		/*
-		* Instancio o conversor e mando ele transformar para dadosSerie e imprimo os dados da série
-		* */
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=f7de918f");
-
-		/*
-		* Usando a classe genérica para converter também os dados dos episódios.
-		* */
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		/*
-		* Convertendo os dados da temporada para classe.
-		* Quero buscar os dados de todas as temporadas. Então meu for irá percorrer o número de temporadas que a série tem.
-		* */
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-		for (int i = 1; i<= dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=f7de918f");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,13 +1,19 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner resposta = new Scanner(System.in);
     private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
 
     /*
     * Como estamos lidando com constantes e que não iremos modificar ao longo do tempo, faremos:
@@ -27,7 +33,25 @@ public class Principal {
         * que estivermos trocando o endereço, deixaremos essa classe como atributo da principal.
         * */
         var json = consumoApi.obterDados(        ENDERECO + nomeDaSerie.replace(" ", "+") + API_KEY);
-                //https://www.omdbapi.com/?t=gilmore+girls&apikey=f7de918f
 
+        /*
+         * Instancio o conversor e mando ele transformar para dadosSerie e imprimo os dados da série
+         * */
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        /*
+         * Convertendo os dados da temporada para classe.
+         * Quero buscar os dados de todas as temporadas. Então meu for irá percorrer o número de temporadas que a série tem.
+         * */
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+		for (int i = 1; i<= dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados(ENDERECO + nomeDaSerie.replace(" ", "+")
+                    + "&season=" + i + API_KEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+        temporadas.forEach(System.out::println);
     }
 }
